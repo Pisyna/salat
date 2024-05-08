211f29e3b7aa09059b41356b33b46a380479d7b8
Pepsi <polina.karpova.2004lao@gmail.com>
2024-05-08 15:33:08 +0300

    gg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
 	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
@@ -13,17 +11,12 @@
 	<h1>
 		<span class="welcome">
 			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
+				<source srcset="lib\images/jgc.jpg" type="image/jpeg">
+				<img src="lib\images/jgc.jpg" alt="Description of the image">
 			</picture>
 		</span>
 
-		to your new<br />SvelteKit app
 	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
 
 	<Counter />
 </section>
--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -1,6 +1,22 @@
 <script>
   import { onMount } from 'svelte';
-  import { db } from './firebase';
+  
+  import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQWBP1WNqa7pLqa5jxsn1qgYoDgndmaGc",
+  authDomain: "salat-6697a.firebaseapp.com",
+  databaseURL: "https://salat-6697a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "salat-6697a",
+  storageBucket: "salat-6697a.appspot.com",
+  messagingSenderId: "830216340439",
+  appId: "1:830216340439:web:f92c0402392977d1290192",
+  measurementId: "G-JKMSJ5VY28"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
 
   let password = '';
 
@@ -26,4 +42,4 @@
 </script>
 
 <input type="password" bind:value={password} placeholder="Enter your password" />
-<button on:click={savePassword}>Save Password</button>
+<button on:click={savePassword}>Сохранить пароль</button>
