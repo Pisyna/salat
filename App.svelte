29c6dc3a453746b2883b0c9479f399dce413c868
Pepsi <polina.karpova.2004lao@gmail.com>
2024-05-08 13:35:20 +0300

    jj

---
+++ App.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from 'svelte';
+    import { db } from './firebase.js';
+  
+    let password = '';
+  
+    const savePassword = () => {
+      db.collection('passwords').add({
+        password: password
+      });
+    };
+  
+    onMount(() => {
+      // При загрузке страницы, подпишитесь на изменения в коллекции "passwords"
+      const unsubscribe = db.collection('passwords').onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+          if (change.type === 'added') {
+            console.log('New password:', change.doc.data());
+          }
+        });
+      });
+  
+      // Не забудьте отменить подписку при размонтировании компонента
+      return () => unsubscribe();
+    });
+  </script>
+  
+  <input type="password" bind:value={password} placeholder="Enter your password" />
+  <button on:click={savePassword}>Save Password</button>
