78110ceab26d4c7011bf3ea8279e90ca637f1ec1
Pepsi <polina.karpova.2004lao@gmail.com>
2024-05-08 15:59:43 +0300

    gg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,12 @@
 <script>
 	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/ct.png';
-	import welcome_fallback from '$lib/images/ct.png';
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/png" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-	</h1>
 <Counter />
 </section>
 
@@ -30,23 +19,4 @@
 		flex: 0.6;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 0;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 0;
-		height: 0;
-		top: 0;
-		display: block;
-	}
 </style>
--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -43,3 +43,21 @@ const db = getFirestore(firebaseApp);
 
 <input type="password" bind:value={password} placeholder="Enter your password" />
 <button on:click={savePassword}>Сохранить пароль</button>
+
+<style>
+	input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  button {
+    background-color: red;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 25px;
+    cursor: pointer;
+  }
+</style>
